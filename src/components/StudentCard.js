f0fe7626e9e82d0166c6789e7e1f1f974661ddb8
gigicupp <gigicupp@gmail.com>
2021-07-25 08:19:10 -0700

    progress on step 4

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import './StudentCard.css';
 
-function StudentCard() {
+function StudentCard({ st, id, isExpanded, handleClick }) {
+  const calculateAvg = (arr) => {
+    let nums = arr.map(str => parseFloat(str))
+    let total = nums.reduce((val, acc) => Number(val) + acc);
+    return total / arr.length;
+  }
+
   return (
-    <div>
-      
+    <div className='StudentCard'>
+      <div className='StudentCard-img'>
+        <img src={st.pic} alt={`pic of ${st.firstName} ${st.lastName}`} />
+      </div>
+      <div className='StudentCard-info'>
+        <h3>{`${st.firstName} ${st.lastName}`}</h3>
+        <p>Email: {st.email}</p>
+        <p>Company: {st.company}</p>
+        <p>Skill: {st.skill}</p>
+        <p>{`${calculateAvg(st.grades)}%`}</p>
+        <div>
+          {isExpanded.expanded && st.id === id
+            ? <ul className='StudentCard-scores'>
+                {st.grades.map((gr, i) => <li key={i}>{`Test ${i + 1}: ${gr}%`}</li>)}
+              </ul>
+            : null
+          }
+        </div>
+      </div>
+      <div onClick={handleClick}>
+        <button id={st.id}>+</button>
+      </div>
     </div>
   )
 }
--- src/components/StudentProfile.js
+++ src/components/StudentProfile.js
@@ -1,38 +1,28 @@
 import React from 'react';
-import useToggleHooks from '../hooks/useToggleHooks';
+import { useState } from 'react';
 import StudentCard from './StudentCard'
 import './StudentProfile.css';
 
 function StudentProfile({ students }) {
-  const calculateAvg = (arr) => {
-    let nums = arr.map(str => parseFloat(str))
-    let total = nums.reduce((val, acc) => Number(val) + acc);
-    return total / arr.length;
+  //expand student detail
+  const [isExpanded, setIsExpanded] = useState({id: 0, expanded: false})
+  const [id, setId] = useState(null)
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsExpanded({id: e.target.id, expanded: !isExpanded.expanded});
+    setId(e.target.id)
   }
 
-   //expand student detail
-   const [isExpanded, setIsExpanded] = useToggleHooks(false)
-
   return (
-    students.map((st)=> {
+    students.map((st) => {
       return <div key={st.id} id={st.id} className='StudentProfile'>
-        <div className='StudentProfile-img'>
-          <img src={st.pic} alt={`pic of ${st.firstName} ${st.lastName}`} />
-        </div>
-        <div className='StudentProfile-info'>
-          <h3>{`${st.firstName} ${st.lastName}`}</h3>
-          <p>Email: {st.email}</p>
-          <p>Company: {st.company}</p>
-          <p>Skill: {st.skill}</p>
-          <p>{`${calculateAvg(st.grades)}%`}</p>
-          {isExpanded 
-            ? <ul className='StudentProfile-scores'>
-                {st.grades.map((gr,i) => <li>{`Test ${i + 1}: ${gr}%`}</li>)}
-              </ul>
-            : null
-          }
-        </div>
-        <button onClick={setIsExpanded}>+</button>
+        <StudentCard 
+          st={st} 
+          handleClick={handleClick} 
+          isExpanded={isExpanded}
+          id={id}
+        />
       </div>
     })
   )
